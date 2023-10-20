3aeff9b2b453ad9e86245d505f56bbf00e8abb47
A-DITYA7070 <99244575+A-DITYA7070@users.noreply.github.com>
2023-10-20 10:33:48 +0530

    added dp problems

--- dp/ONE_D_DP/climbing_stairs.cpp
+++ dp/ONE_D_DP/climbing_stairs.cpp
@@ -1,5 +1,25 @@
+/**
+ * @file climbing_stairs.cpp
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2023-10-20
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+/**
+    Problem Statement: Given a number of stairs. Starting from the 0th stair we need to climb to the “Nth” stair. 
+                       At a time we can climb either one or two steps. We need to 
+                       return the total number of distinct ways to reach from 0th to Nth stair.
+ *
+ */
+
+
 #include<bits/stdc++.h>
 using namespace std;
+
 class ONEDDP{
     public:
     /**
